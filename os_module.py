cd03f9364285c89fa7ff5a000a5e4261680efc36
Lawrence <lawrencelanzaderas020497@gmail.com>
2020-08-24 23:13:00 +0800

    Test

--- os_module.py
+++ os_module.py
@@ -8,7 +8,6 @@ if not os.path.exists("OS_MODULE_SAMPLE"):
 else:
     print("Meron ng OS_MODULE_SAMPLE")
 
-
 hold_cwd = os.path.abspath("OS_MODULE_SAMPLE")
 print("hold cwd>", hold_cwd)
 if not os.path.exists("" + hold_cwd + "\\INSIDE_OS_MODULE_SAMPLE"):
